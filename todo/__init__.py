4f34d0638c62d2c2a18b84ed48eef11440c4541b
dimon <xlxdimsanxlx@gmail.com>
2021-12-20 20:25:39 +0200

    diploma

--- todo/__init__.py
+++ todo/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+from anons import announcement
+
 UPLOAD_FOLDER = 'home/dimon/diplom_beetroot/static'
 
 app = Flask(__name__)
+app.register_blueprint(announcement)
 app.secret_key = 'diploma beetroot academy'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# app.register_blueprint(routs)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/dimon/diplom_beetroot/data_base/db_user.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
